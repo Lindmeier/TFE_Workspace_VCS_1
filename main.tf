3e41f88533c7164b1caf93a2decf5fb43a574c01
xgadlnd <Jan.Lindmeier@atruvia.de>
2022-07-12 13:12:04 +0200

    use new Module terraform-fakewebservices-server - corrected vara Name

--- main.tf
+++ main.tf
@@ -7,6 +7,6 @@ module "test" {
   source  = "app.terraform.io/team-automation/server/fakewebservices"
   version = "0.0.1"
   # insert required variables here
-  serverSount = 2
+  serverCount = 2
   dbSize = 512
 }
